389f929e03aa7e0adc4313cc8240ac4aaccfa586
Vivean28 <vivean.la@gmail.com>
2021-05-10 09:51:37 -0700

    week-1

--- coding-concepts.js
+++ coding-concepts.js
@@ -5,41 +5,44 @@
 // First, predict the outcome based on your understanding of the code.
 // Then, uncomment the console.log() and verify the output. Briefly explain why your initial answer was correct or incorrect.
 
-// --------------------1) What will this log?
+// --------------------1) What will this log? 
+
+
 
 var cohort = "Bravo 2021"
-// console.log(cohort.length)
+ console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:It will log Bravo 2021.
+// b) Verify and explain:I did not pay atention to .length so the actual out put was 10
 
 
 // --------------------2) What will this log?
+//This will log l
 
 var greeting = "Hello World!"
-// console.log(greeting[3])
+ console.log(greeting[3])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: This will log l
+// b) Verify and explain: I will log l because it is asking for the index of 3 
 
 
 // --------------------3) What will this log?
 
 var languages = ["JavaScript", "Ruby", "Python", "C++"]
 var index = 1
-// console.log(languages[index])
+ console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: I will log ["JavaScript", "Ruby", "Python", "C++"] and the 1
+// b) Verify and explain: I was worng, the it logged Ruby. I dont understand why 
 
 
 // --------------------4) What will this log?
 
 var weekendDays = ["saturday", "sunday"]
-// console.log(weekendDays.toUpperCase())
+ console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: It will log all the array of strings in upper case letters SATURDAY SUNDAY
+// b) Verify and explain This did not log not sure why
 
 
 // --------------------5) What will this log?
@@ -47,5 +50,5 @@ var weekendDays = ["saturday", "sunday"]
 var dataTypes = ["number", "string", "Boolean"]
 // console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:It will give me the type of data type this is which is a number and the #3
+// b) Verify and explain: number
